e8070ea6ea1fa3b21dbd3a16668d9533a5add2a1
Yurii Koryshko <152334691+KoryshCWE@users.noreply.github.com>
2024-08-12 04:16:52 -0700

    module_17

---
+++ src/main/resources/db/migration/V2__Add_users_and_roles.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+                                     username VARCHAR(50) PRIMARY KEY,
+                                     password VARCHAR(100) NOT NULL,
+                                     enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authorities (
+                                           username VARCHAR(50) NOT NULL,
+                                           authority VARCHAR(50) NOT NULL,
+                                           FOREIGN KEY (username) REFERENCES users(username)
+);
+
+INSERT INTO users (username, password, enabled)
+VALUES ('user', '{noop}jdbcDefault', true);
+
+INSERT INTO authorities (username, authority)
+VALUES ('user', 'ROLE_USER');
